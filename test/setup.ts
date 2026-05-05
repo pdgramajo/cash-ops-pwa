9ab5a4201aa49b3ed5bde3f76f118620946d7510
pdgramajo <pdgramajo@github.com>
2026-05-05 18:00:55 -0300

    fix: Fix CI test command and fake-indexeddb setup

--- test/setup.ts
+++ test/setup.ts
@@ -1,6 +1,9 @@
 import '@testing-library/jest-dom'
 import { beforeAll, afterEach, afterAll, vi } from 'vitest'
 import { cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { beforeAll, afterEach, afterAll, vi } from 'vitest'
+import { cleanup } from '@testing-library/react'
 import 'fake-indexeddb/auto'
 
 beforeAll(async () => {
