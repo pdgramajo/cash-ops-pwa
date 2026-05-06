c5758f6e4c71095714ba34e2e37e5a21b40d7c44
pdgramajo <pdgramajo@github.com>
2026-05-06 00:45:04 -0300

    fix: Update App.tsx to use AppRouter with all routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AppRouter } from './AppRouter';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={
-          <div style={{ padding: '20px', textAlign: 'center' }}>
-          <h1>Cash Operations System</h1>
-          <p>Loading...</p>
-        </div>
-        } />
-      </Routes>
-    </BrowserRouter>
-  )
+  return <AppRouter />;
 }
 
-export default App
+export default App;
